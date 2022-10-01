d43b3d505a562ddbfc0a400b5617dcc69726028f
nt-erdenetugs <erdenetugs@nashatech.com>
2022-10-01 13:03:22 +0800

    added

--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,17 @@
 <template>
+  <div>
     <h1>Test page</h1>
+    <p>{{ members }}</p>
+  </div>
 </template>
+
+<script>
+import members from '../localDB/members.json'
+export default {
+  data() {
+    return {
+      members,
+    }
+  },
+}
+</script>
